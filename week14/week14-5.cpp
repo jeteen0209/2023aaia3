fd0bfd41ef6d377fe1c82579ab582fd609271c65
jeteen0209 <jeteen501@gmail.com>
2024-12-12 16:09:08 +0800

    week14

---
+++ week14/week14-5.cpp
@@ -0,0 +1,16 @@
+//week14-5.cpp not ok
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	while(cin>>a>>b){
+		while(a>0){
+			cout << a << " ";
+			a = a / b;
+		}
+
+		cout<<"Boring!\n";
+	}
+}
